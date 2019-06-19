52a8611d949a06afe9cabf52d1621eb364287d3f
Diya Li <dli@esri.com>
2019-06-19 16:01:30 +0800

    add subprocess

--- keepRun.py
+++ keepRun.py
@@ -1,6 +1,21 @@
-import os 
+# # run 1
+# import os 
+# cmd = 'node screenshot-invoke.js > temp.txt'
 
-cmd = 'node screenshot-invoke.js > temp.txt'
+# for i in range(100):
+#     os.system(cmd).read()
+
+
+
+
+# run 2
+import subprocess
+cmd2 = 'node screenshot-invoke.js'.split(" ")
+
 
 for i in range(100):
-    os.system(cmd).read()
+    subPopen = subprocess.Popen(cmd2,
+                stdout=subprocess.PIPE, 
+                stderr=subprocess.STDOUT)
+    stdout, stderr = subPopen.communicate()
+    print(stdout, stderr)
